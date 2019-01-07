9e40fe61f9acdce64bf00b7dbc41a78047bed2c7
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2019-01-07 12:14:09 +0000

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,7 @@ import { ListItem } from 'react-native-elements';
 import { Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -54,12 +55,37 @@ class About extends Component{
   };
     
     render(){
-        return(
-        <ScrollView>
-            <History />
-            <Leadership leaders = {this.props.leaders.leaders} />
-        </ScrollView>
-        );
+         if (this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if (this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return(
+            <ScrollView>
+                <History />
+                <Leadership leaders = {this.props.leaders.leaders} />
+            </ScrollView>
+        ); 
+        }
+        
     }
 }
 
